3cd77fc3c8abd694f9b020bcde814d69e70b0bc2
Jesus <jesus.guetio@correounivalle.edu.co>
2024-05-04 13:42:05 -0500

    round trip data added to response getflightsview

--- flights/filters.py
+++
@@ -1,8 +0,0 @@
-from rest_framework.filters import BaseFilterBackend
-
-class CustomFilter(BaseFilterBackend):
-    def filter_queryset(self, request, queryset, view):
-        airline = request.query_pararms.get('airline', None)
-        if airline:
-            queryset = [item for item in queryset if item['airline']==airline]
-        return queryset
